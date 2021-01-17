12e2953770481cd7e19c7519d8e80b291d09c451
Atsushi Nakamura <atsushi_a_nakamura@jp.ricoh.com>
2021-01-18 08:30:41 +0900

    Delete unnecessary comment.

--- Source/GenerateSource/StructObject.cs
+++ Source/GenerateSource/StructObject.cs
@@ -9,17 +9,12 @@ namespace GenerateSource
         [CompareBy]
         public int Value1 { get; set; }
 
-        [CompareBy(Priority = 2)]
-        public int Value2 { get; set; }
+        [CompareBy(Priority = 2)] 
+        public int Value2;
 
         [CompareBy(Priority = 1)]
         public int Value3 { get; set; }
 
         public int NotApplicable { get; set; }
-
-        //public int CompareTo(object obj)
-        //{
-        //    throw new NotImplementedException();
-        //}
     }
 }
