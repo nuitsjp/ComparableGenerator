09c772050b7cd98c78227641f5dc63e07c7fff32
Atsushi Nakamura <nuits.jp@live.jp>
2021-02-07 08:52:05 +0900

    Refactor for Analyzer unit test.

---
+++ Source/ComparableGenerator.UnitTest/Extensions/AnalyzerExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using ComparableGenerator.UnitTest.Verifiers;
+using FluentAssertions.Primitives;
+using Microsoft.CodeAnalysis.Testing;
+
+// ReSharper disable once CheckNamespace
+namespace ComparableGenerator.UnitTest
+{
+    public static class AnalyzerExtensions
+    {
+        public class Analyzer
+        {
+            public string Source { get; }
+
+            public Analyzer(string source)
+            {
+                Source = source;
+            }
+        }
+
+        public static Analyzer CreateAnalyzer(this string source) => new Analyzer(source);
+
+        public static AnalyzerAssertions Should(this Analyzer instance)
+        {
+            return new AnalyzerAssertions(instance);
+        }
+
+
+        public class AnalyzerAssertions : ReferenceTypeAssertions<Analyzer, AnalyzerAssertions>
+        {
+            public AnalyzerAssertions(Analyzer instance)
+            {
+                Subject = instance;
+            }
+
+            protected override string Identifier => "analyzer";
+
+            public async Task BeGeneratedDiagnosticsAsync(params DiagnosticResult[] expected)
+            {
+                await CSharpAnalyzerVerifier<SourceAnalyzer>.VerifyAnalyzerAsync(Subject.Source, expected);
+            }
+
+            public async Task NotBeGeneratedDiagnosticsAsync()
+            {
+                await CSharpAnalyzerVerifier<SourceAnalyzer>.VerifyAnalyzerAsync(Subject.Source);
+            }
+        }
+    }
+}
--- Source/ComparableGenerator.UnitTest/SourceAnalyzerTest.cs
+++ Source/ComparableGenerator.UnitTest/SourceAnalyzerTest.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
 using Microsoft.CodeAnalysis;
 using Xunit;
 using VerifyCS = ComparableGenerator.UnitTest.Verifiers.CSharpAnalyzerVerifier<
@@ -10,17 +12,20 @@ namespace ComparableGenerator.UnitTest
     {
         public override async Task Should_not_be_generated_for_CompareAttribute_is_not_defined(string source)
         {
-            await VerifyCS.VerifyAnalyzerAsync(source);
+            await source.CreateAnalyzer()
+                .Should().NotBeGeneratedDiagnosticsAsync();
         }
 
         public override async Task Should_be_generated_for_class(string source)
         {
-            await VerifyCS.VerifyAnalyzerAsync(source);
+            await source.CreateAnalyzer()
+                .Should().NotBeGeneratedDiagnosticsAsync();
         }
 
         public override async Task Should_be_generated_for_struct(string source)
         {
-            await VerifyCS.VerifyAnalyzerAsync(source);
+            await source.CreateAnalyzer()
+                .Should().NotBeGeneratedDiagnosticsAsync();
         }
 
         public override async Task Should_not_be_generated_When_not_exists_CompareBy(string source)
@@ -30,7 +35,8 @@ namespace ComparableGenerator.UnitTest
                     .Diagnostic(SourceAnalyzer.CompareByIsNotDefined.Rule)
                     .WithLocation(7, 18)
                     .WithArguments("MyNamespace", "MyClass");
-            await VerifyCS.VerifyAnalyzerAsync(source, expected);
+            await source.CreateAnalyzer()
+                .Should().BeGeneratedDiagnosticsAsync(expected);
         }
     }
 }
