f12d8121136a4dff87f618535d366a547ce8da0d
Atsushi Nakamura <nuits.jp@live.jp>
2021-02-11 05:45:44 +0900

    Add SourceFixProvider.

---
+++ Source/ComparableGenerator/SourceFixProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+
+namespace ComparableGenerator
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(SourceFixProvider)), Shared]
+    public class SourceFixProvider : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray<string>.Empty;
+
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
