e5a9670a574760fdbfe98f577a9952386543cf48
Atsushi Nakamura <atsushi_a_nakamura@jp.ricoh.com>
2021-02-16 14:42:28 +0900

    Add SampleConsoleApp.

---
+++ Source/SampleConsoleApp/Employee.cs
@@ -0,0 +1,14 @@
+﻿using ComparableGenerator;
+
+namespace SampleConsoleApp
+{
+    [Comparable]
+    public partial class Employee
+    {
+        [CompareBy(Priority = 2)]
+        public string FirstName { get; set; }
+
+        [CompareBy(Priority = 1)]
+        public string LastName { get; set; }
+    }
+}
---
+++ Source/SampleConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = new List<Employee>();
+            employees.Add(new Employee {LastName = "Tanaka", FirstName = "Taro"});
+            employees.Add(new Employee {LastName = "Suzuki", FirstName = "Jiroh"});
+            employees.Add(new Employee {LastName = "Suzuki", FirstName = "Ichiroh"});
+
+            employees.Sort();
+
+            employees.ForEach(x => Console.WriteLine($"{x.LastName} {x.FirstName}"));
+        }
+    }
+}
